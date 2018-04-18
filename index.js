0f1de1424687d614fd620a8436c5266413b30b85
Onur Elibol <korax11@hotmail.com>
2018-04-19 00:44:49 +0200

    WIP: Selenium automation

--- index.js
+++ index.js
@@ -16,8 +16,43 @@ app.use((err, req, res, next) => {
   }
 });
 
+const zip = process.cwd() + "/test_files/160x600.zip";
+const { Builder, By, Key, until } = require("selenium-webdriver");
+
+// Selenium driver
+const driver = new Builder()
+  .usingServer()
+  .withCapabilities({
+    browserName: "chrome"
+  })
+  .build();
+
+async function start() {
+  driver.get("http://studio.adform.com/browse-templates/");
+  const fileInput = await driver.findElement(By.css("input[type=file]"));
+  fileInput.sendKeys(zip);
+
+  // Wait until Category select box to show up
+  await driver.wait(until.elementLocated(By.css("[data-select-id='adf-select-2570']")), 5000);
+  //  Click Category selectbox > Click Display
+  await driver.findElement(By.css("[data-select-id='adf-select-2570']")).click();
+  await driver.findElement(By.xpath("//*[@id='adf-select-2570']/li[1]")).click();
+
+  // Click Format selectbox > Click Standard
+  await driver.findElement(By.css("[data-select-id='adf-select-2573']")).click();
+  await driver.findElement(By.xpath("//*[@id='adf-select-2573']/li[1]")).click();
+
+  // Click Upload
+  await driver.findElement(By.xpath("//div[@class='adf-BannerFormatSelect']/div[3]/button[1]")).click();
+
+  Logger.log("I'm done so far.", "magenta");
+  // driver.quit();
+}
+
 const server = app.listen(3000, () => {
-  Logger.log(`App is running at port: ${server.address().port}`, "green");
+  Logger.log(`[app] Running at port: ${server.address().port}`, "green");
+  // Scrape logic
+  start();
 });
 
 // CTRL + C hook
