cc026918b0adc840ec9e03c86709d5458ced1b5f
Onur Elibol <korax11@hotmail.com>
2018-04-19 23:22:58 +0200

    Open multiple instances parallel / Start driver with specific prefs

--- index.js
+++ index.js
@@ -16,21 +16,29 @@ app.use((err, req, res, next) => {
   }
 });
 
-const zip = process.cwd() + "/test_files/160x600.zip";
-const { Builder, By, Key, until } = require("selenium-webdriver");
+const { Builder, By, until } = require("selenium-webdriver");
 
-// Selenium driver
-const driver = new Builder()
-  .usingServer()
-  .withCapabilities({
-    browserName: "chrome"
-  })
-  .build();
+// Download path for files
+const downloadPath = "/Users/Onur/Desktop";
 
-async function start() {
+async function start(x) {
+  // Selenium driver (Declare here to have multiple instances)
+  const driver = new Builder()
+    .usingServer()
+    .withCapabilities({
+      browserName: "chrome",
+      chromeOptions: {
+        prefs: {
+          download: { default_directory: downloadPath, directory_upgrade: true, extensions_to_open: "" }
+        }
+        // args: ["--test-type", "incognito"]     // Arguments to send chrome.app
+      }
+    })
+    .build();
+
   driver.get("http://studio.adform.com/browse-templates/");
   const fileInput = await driver.findElement(By.css("input[type=file]"));
-  fileInput.sendKeys(zip);
+  fileInput.sendKeys(process.cwd() + "/test_files/" + x);
 
   // Wait until Category select box to show up
   await driver.wait(until.elementLocated(By.css("[data-select-id='adf-select-2570']")), 5000);
@@ -63,15 +71,19 @@ async function start() {
   const exportButton = driver.findElement(By.xpath("//*[contains(text(), 'Export')]"));
   driver.executeScript("arguments[0].click();", exportButton);
 
-
   Logger.log("I'm done so far.", "magenta");
-  // driver.quit();
+  driver.quit();
 }
 
+// AppStart
 const server = app.listen(3000, () => {
   Logger.log(`[app] Running at port: ${server.address().port}`, "green");
-  // Scrape logic
-  start();
+
+  // Entry point
+  const fileList = ["160x600.zip", "180x500.zip"];
+  fileList.forEach((file) => {
+    start(file);
+  });
 });
 
 // CTRL + C hook
