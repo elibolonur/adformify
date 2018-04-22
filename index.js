876aca8097c3c48c1b1fceee1cb477cbb4628e02
Onur Elibol <korax11@hotmail.com>
2018-04-22 16:14:55 +0200

    Move selenium logic to the class / unblock main thread

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import Logger from "./lib/logger.js";
 import { getFilesSync } from "./lib/helpers";
-import { AdForms } from "./lib/selenium";
+import { SeleniumRunner } from "./lib/selenium";
 
 const app = express()
   .use(bodyParser.json())
@@ -22,12 +22,9 @@ app.use((err, req, res, next) => {
 const server = app.listen(3000, async() => {
   Logger.log(`[app] Running at port: ${server.address().port}`, "green");
   const fileList = getFilesSync("./test_files");
-  const adforms = new AdForms();
+  const seleniumRunner = new SeleniumRunner(fileList, 2);
 
-  for (const file of fileList) {
-    await adforms.execute(file);
-  }
-  adforms.quit();
+  seleniumRunner.execute();
 });
 
 // CTRL + C hook
